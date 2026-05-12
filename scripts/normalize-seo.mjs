12c25e2009c952ef86919deeaec10304e54f4ac6
louisdicrespo27-cloud <louisdicrespo27@gmail.com>
2026-05-12 22:38:46 +0100

    1.2

--- scripts/normalize-seo.mjs
+++ scripts/normalize-seo.mjs
@@ -137,7 +137,7 @@ function main() {
   const xml =
     `<?xml version="1.0" encoding="UTF-8"?>\n` +
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
-    urls.map((u) => `  <url>\n    <loc>${u.loc}</loc>\n    <lastmod>${u.lastmod}</lastmod>\n  </url>`).join('\n') +
+    urls.map((u) => `  <url>\n    <loc>${u.loc}</loc>\n    <lastmod>${u.lastmod}</lastmod>\n    <changefreq>monthly</changefreq>\n  </url>`).join('\n') +
     `\n</urlset>\n`;
   fs.writeFileSync(path.join(ROOT, 'sitemap.xml'), xml);
   console.log(`sitemap.xml: ${urls.length} URLs.`);
